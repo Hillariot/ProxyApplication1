5dc0376ed41f41d462e47ac866e7c87a11766ebc
Hillariot <hillariot2070@gmail.com>
2025-02-26 21:21:13 +0400

    First loading
    Application - first loading

---
+++ VPN_Application/VPN_Application/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VPN_Application
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ VPN_Application/VPN_Application/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VPN_Application
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
